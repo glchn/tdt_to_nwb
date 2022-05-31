ba423dfab624b8b309d1a85ba803be6dca2447e4
Gale Chen <61856642+glchn@users.noreply.github.com>
2022-05-31 14:56:01 -0700

    tdt tutorial

--- convert.py
+++ convert.py
@@ -1,15 +1,29 @@
 import tdt
 from pynwb import NWBHDF5IO
 import numpy as np
+import matplotlib.pyplot as plt
 
+# this does something! i don't know what.
 def xt(arr, fs, axis=0):
-    return np.arange(0, (arr.shape[axis])/fs, 1/fs)
+    retval = np.arange(0, (arr.shape[axis])/fs, 1/fs)
+    print("length of retval: " + str(len(retval)) + "\nretval is: " + str(retval))
+    return retval
+
+# plot xt.
+# def plotsimple(arrx, arry):
+#     plt.plot(arrx, arry)
+#     plt.axis([min(arrx), max(arrx), min(arry), max(arry)])
+#     plt.xlabel('x - axis')
+#     plt.ylabel('test')
+#     plt.savefig("output2.jpg")
 
 def get_fiber_data(data_block):
     reader = tdt.TDTbin2py
     data = reader.read_block(data_block)
+    print("\ndata is: " + str(data))
 
     t = xt(data.streams._465A.data, data.streams._465A.fs)
+    # plotsimple(t, t/1)
     led_on = data.epocs.TIG_.onset[1]
     led_off = data.epocs.TIG_.offset[1]
     t_on = np.argmin(np.abs(t-led_on))
@@ -27,15 +41,8 @@ def get_fiber_data(data_block):
     return data.streams._465A.data,  data.streams._405A.data, data.streams._465A.fs, t_on, t_off
 
 data_block = r'/mnt/c/Users/scsc7/Documents/Github/tdt_to_nwb/test'
-data465, data405, fs, t_on, t_off = get_fiber_data(data_block)
+# data465, data405, fs, t_on, t_off = get_fiber_data(data_block)
 
 #   File "./convert.py", line 12, in get_fiber_data
 #     t = xt(data.streams._466A.data, data.streams._466A.fs)
 # AttributeError: 'StructType' object has no attribute '_466A'
-
-# print("\nCONVERTING from tdt to nwb\n")
-
-# with NWBHDF5IO("FibPhoTest-220218-125509_test-220523-190029.nwb", "r") as io:
-#     read_nwbfile = io.read()
-#     print("\nPRINTING nwb file contents\n")
-#     print(read_nwbfile)
