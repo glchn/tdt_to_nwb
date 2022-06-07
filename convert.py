8eae05579d44eba31fe639370ef26bc647344af2
Gale Chen <61856642+glchn@users.noreply.github.com>
2022-06-07 13:55:54 -0700

    nwb file check outputting plots

--- convert.py
+++ convert.py
@@ -1,26 +1,50 @@
-from tdt import read_block, read_sev, epoc_filter
+from tdt import read_block
 import numpy as np
 import matplotlib.pyplot as plt
-
-from qtpy.QtWidgets import QFileDialog, QApplication
-from os import getcwd, path, listdir
-import sys
+import os
+from os import getcwd, path, listdir, remove
 from datetime import datetime
 from dateutil.tz import tzlocal
 import numpy as np
-import re
-
-from pynwb import NWBFile, NWBHDF5IO, ProcessingModule, file, TimeSeries
-from pynwb.epoch import TimeIntervals
-from pynwb.ecephys import ElectricalSeries, FilteredEphys, SpikeEventSeries, EventWaveform
-from pynwb.behavior import BehavioralTimeSeries, EyeTracking, PupilTracking
-from pynwb.misc import AbstractFeatureSeries
+from pynwb import NWBFile, NWBHDF5IO, TimeSeries
 
 data_block = r'/mnt/c/Users/scsc7/Documents/Github/tdt_to_nwb/test'
-nwb_name = 'test.nwb'
+working_dir = r'/mnt/c/Users/scsc7/Documents/Github/tdt_to_nwb/'
 
-def new_nwb(tdt_data):
+# plot time series data of a nwb file
+# param: nwb_in stream reader for nwb file we're plotting, store being plotted
+def testplot(nwb_in, store):
+    store_timeseries_entry = nwb_in.acquisition["timeseries"+str(store)]
+    num_points = len(store_timeseries_entry.data[:])
+    nwb_sampling_rate = store_timeseries_entry.rate
+    time = np.linspace(1, num_points, num_points) / nwb_sampling_rate
+    t = int(num_points * nwb_sampling_rate) # int rounds it to the nearest integer
+    plt.plot(time[0:t], store_timeseries_entry.data[0:t], color='cornflowerblue') # plot the line using slices
+    plt.title(str([store]) + " Data", fontsize=16) # create title, axis labels
+    plt.xlabel('Seconds', fontsize=14)
+    plt.ylabel('Volts', fontsize=14)
+    plt.autoscale(tight=True)
+    plt.savefig("nwb" + str([store]) + ".jpg")
+    print("done plotting iteration" + str([store]) + "\n") # debug statement
+
+# use to check if nwb file was correctly written. prints acquisition info and timeseries data. plots data.
+# params: name of nwb file you'll be reading, tdt data used to grab the store info.
+def checkwrite(nwb_name, tdt_data):
+    nwb_io = NWBHDF5IO(nwb_name, 'r')
+    nwb_in = nwb_io.read()
+    print(nwb_in.acquisition)
+    for store in tdt_data.streams.keys():
+        print(nwb_in.acquisition["timeseries" + str(store)].data[:])
+        testplot(nwb_in, store)
+
+# creates new nwb file with basic properties
+# param: tdt file from which data is retrieved, name of nwb file to create and write to
+# TODO: add params for basic properties. not sure if these come from tdt file
+def new_nwb(tdt_data, nwb_name):
     nwb_io = []
+    if path.isfile(working_dir + nwb_name): # if file already exists, delete it
+        os.remove(working_dir + nwb_name) # nwb doesn't like it when you re-write the metadata
+        # print("file removed\n") # debug statement
     nwb_io = NWBHDF5IO(nwb_name, mode='w')
     nwb_file = NWBFile(session_description ="place holder",
                         identifier = "place holder",
@@ -34,16 +58,14 @@ def new_nwb(tdt_data):
                         source_script='convert.py',
                         source_script_file_name='convert.py',
                         notes="")
-    new_time_series(nwb_file, tdt_data)
+    new_time_series(nwb_file, tdt_data) # add time series from tdt data
     nwb_io.write(nwb_file)
     nwb_io.close()
 
-# iterative time series data for each stream
+# creates a time series for each store
+# params: nwb_file to be written to, tdt_data from which timeseries is extracted
 def new_time_series(nwb_file, tdt_data):
     for store in tdt_data.streams.keys():
-        # print("\ntimeseries" + str(store))
-        # print(tdt_data.streams[store].data[:])
-        # print(tdt_data.streams[store].fs)
         time_series_with_rate = TimeSeries(
             name = ("timeseries" + str(store)),
             data = tdt_data.streams[store].data[:],
@@ -54,19 +76,11 @@ def new_time_series(nwb_file, tdt_data):
         nwb_file.add_acquisition(time_series_with_rate)
 
 # converts tdt files to nwb file
-# params: string block_path is path to tdt folder
-#         string nwb_file_name is name of nwb file you will write to
-def convert():
+# params: string nwb_file_name is name of nwb file you will write to
+def convert(nwb_name):
     data = read_block(data_block)
-    # print("\ndata:\n" + str(data))
-    # print("\ninfo:\n"+ str(data.info))
-    # print ('\nall stream stores: \n' + str(data.streams))
-    new_nwb(data)
-    # print('\nChannel data in', data.info.blockname)
-    # for channel in data.streams.keys():
-    #     print(channel, data.streams[channel])
-    # print(store, data.streams[store].data[:])
-    # print('\nSampling rates in', data.info.blockname)
-    # for store in data.streams.keys():
-    #     print(store, '{:.4f} Hz'.format(data.streams[store].fs))
-convert()
+    new_nwb(data, nwb_name)
+
+convert('test.nwb') # write tdt data to nwb data
+checkwrite('test.nwb', read_block(data_block)) # test read to check if data was successfully written
+#TODO: stuck on Fi2r and Fi1r... why?
--- tdt_reader/readtdt.py
+++ tdt_reader/readtdt.py
@@ -1,21 +1,18 @@
 from tkinter import N
 import matplotlib.pyplot as plt  # standard Python plotting library
 import numpy as np  # fundamental package for scientific computing, handles arrays and maths
-# import the primary functions from the tdt library only
 from tdt import read_block
-
-# TODO: Why is script not terminating? fi1r and fi2r
 
 block_path = r'/mnt/c/Users/scsc7/Documents/Github/tdt_to_nwb/test'
 
+# produce plot for each stream
+# param: path of tdt data
 def plot_streams(block_path):
     data = read_block(block_path)
-
-    print ('\nall stream stores: \n' + str(data.streams)) # debugging print statements
-    print('\nChannel data in', data.info.blockname)
-    for channel in data.streams.keys():
-        print(channel, data.streams[channel])
-
+    # print ('\nall stream stores: \n' + str(data.streams)) # debugging print statements
+    # print('\nChannel data in', data.info.blockname)
+    # for channel in data.streams.keys():
+    #     print(channel, data.streams[channel])
 
     for store in data.streams.keys(): # for each signal and panel
         num_points = len(data.streams[store].data[:]) # get the number of data points
@@ -24,11 +21,12 @@ def plot_streams(block_path):
         t = int(num_points * data.streams[store].fs) # int rounds it to the nearest integer
         fig1 = plt.subplots(figsize=(10, 6)) # declare figure size
         plt.plot(time[0:t], data.streams[store].data[0:t], color='cornflowerblue') # plot the line using slices
-        plt.title(str([store]) + " Data", fontsize=16) # create title, axis labels, and legend
+        plt.title(str([store]) + " Data", fontsize=16) # create title, axis labels
         plt.xlabel('Seconds', fontsize=14)
         plt.ylabel('Volts', fontsize=14)
         plt.autoscale(tight=True)
         plt.savefig("readtdt" + str([store]) + ".jpg")
         print("done iteration" + str([store]) + "\n") # print statement for debugging. getting stuck on Fi2r and Fi1r
+        #TODO: stuck on Fi2r and Fi1r... why?
 
 plot_streams(block_path)
